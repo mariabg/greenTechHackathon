771113054f0b5760cf193f68731bc37c9844e01a
Maria Benavente <mariabenaventeg@gmail.com>
2018-03-09 13:47:38 +0100

    debugging js

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-'use strict';
-const Alexa = require("alexa-sdk");
+// 'use strict';
+// const Alexa = require("alexa-sdk");
+
 alert("hey you..");
-
-exports.handler = function(event, context, callback) {
-    const alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
-
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('SayFact');
-    },
-    'SayFact': function () {
-        this.response.speak('I am still not very clever!');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = 'This is the Hello World Sample Skill. ';
-        const reprompt = 'Say hello, to hear me speak.';
-
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak('Goodbye!');
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function () {
-        this.response.speak('See you later!');
-        this.emit(':responseReady');
-    }
-};
+//
+// exports.handler = function(event, context, callback) {
+//     const alexa = Alexa.handler(event, context);
+//     alexa.registerHandlers(handlers);
+//     alexa.execute();
+// };
+//
+// const handlers = {
+//     'LaunchRequest': function () {
+//         this.emit('SayHello');
+//     },
+//     'GetNewFactIntent': function () {
+//         this.emit('SayFact');
+//     },
+//     'SayFact': function () {
+//         this.response.speak('I am still not very clever!');
+//         this.emit(':responseReady');
+//     },
+//     'AMAZON.HelpIntent': function () {
+//         const speechOutput = 'This is the Hello World Sample Skill. ';
+//         const reprompt = 'Say hello, to hear me speak.';
+//
+//         this.response.speak(speechOutput).listen(reprompt);
+//         this.emit(':responseReady');
+//     },
+//     'AMAZON.CancelIntent': function () {
+//         this.response.speak('Goodbye!');
+//         this.emit(':responseReady');
+//     },
+//     'AMAZON.StopIntent': function () {
+//         this.response.speak('See you later!');
+//         this.emit(':responseReady');
+//     }
+// };
